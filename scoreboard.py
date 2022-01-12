db65189a6d9f30b33000d5e46f7c8789e0563267
mrcruz117 <mrcruz1989@gmail.com>
2022-01-12 13:59:01 -0500

    cars speed up after successful crossing.

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,23 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        self.level = 1
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.goto(-280, 250)
+        self.write(f"level: {self.level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.clear()
+        self.write(f"level: {self.level}", align="left", font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"Game Over...", align="center", font=FONT)
